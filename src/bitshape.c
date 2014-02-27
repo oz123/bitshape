a8a26e2faca3c5476ed4764544cea0bda1981b8f
oz123 <nahumoz@gmail.com>
2014-02-27 20:36:07 +0100

    Add functions to read user input for game 1

--- src/bitshape.c
+++ src/bitshape.c
@@ -5,54 +5,11 @@
 
 void mainloop(void);
 void random_number(void);
+void printbitssimple(int);
 int  compare_user_input(char*);
 
+int guess_num_from_binary(void);
 
-/* Print n as a binary number */
-void printbitssimple(int n) 
-{
-    unsigned int i;
-    i = 1<<(sizeof(n) * 8 - 1);
-    printf("%i\n", i);
-    printf("%i\n", n);
-    while (i > 0) 
-    {
-        if (n & i)  /* check if any of the bits of n is not 0 .*/
-            printf("1");
-        else
-            printf("0");
-        i >>= 1;
-    }
-}
-
-/*
-int main(int argc, char *argv[]) 
-{
-    int i,t;
-    int response;
-    int *answer;
-    int *op1;
-    int *op2;
-    char *result;
-    int x;
-
-    srand(time(NULL));
-
-    printf("\n\tThe BitShape Game");
-    printf("\n1\tGuess the bit shape of integer");
-    printf("\n2\tGuess an integer from a bit shape");
-    printf("Select an option: ");
-    scanf("%d", &response);
-
-    if ( response )
-    for (i = 0; i < 2; ++i) 
-    {
-        printbitssimple(i);
-        printf("\n");
-    }
-        
-    return 0;
-}*/
 
 int main(void) 
 {
@@ -61,21 +18,21 @@ int random_num = 0;
 int guessed_num = 0;
 int counter = 0; 
 int play_on = 1;
-srand(time(NULL));
-random_num = rand() % 1000 + 1;
 char response[CHARSIZE];
 
 printf("\n\t The BitShape Game \n"); 
-
     while(play_on)
     {
+        printf("Enter a choice, which game you would like to play:\n");
+        printf("1 - Guess a number from it's bit shape ...\n");
+        printf("2 - Given an integer type in it's bit shape ...2\n");
         fgets(response, CHARSIZE, stdin);
         printf("Printing s: %s\n", response);
-
         switch(atoi(response)){
             
             case 1:
-                printf("Play version 1\n");
+                printf("Guess a number from it's bit shape ...\n");
+                guess_num_from_binary();
                 break;
             case 2:
                 printf("Play version 2\n");
@@ -91,4 +48,34 @@ printf("\n\t The BitShape Game \n");
         }
     }
     return 0;   
+}
+
+/* Print n as a binary number */
+void printbitssimple(int n) 
+{
+    unsigned int i;
+    i = 1<<(sizeof(n) * 4 - 1);
+    while (i > 0) 
+    {
+        if (n & i)  /* check if any of the bits of n is not 0 .*/
+            printf("1");
+        else
+            printf("0");
+        i >>= 1;
+    }
+}
+/*show the user a bit shape and compare the input*/
+int guess_num_from_binary(void){
+    int random_num ;
+    char ans[8];
+    srand(time(NULL));
+    random_num = rand() % 1000 + 1;
+    
+    printf("Here is the number:\n");
+    printbitssimple(random_num);
+    printf("\nEnter your answer\n");
+    fgets(ans, 2, stdin);
+    printf("\n");
+    printf("you typed: %s \n", ans);
+    return 1;
 }
